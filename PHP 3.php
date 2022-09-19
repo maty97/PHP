67135e5981ef04f07e7f9f70d783599ce18eaee1
maty97 <90652709+maty97@users.noreply.github.com>
2022-09-19 19:49:21 -0300

    Add files via upload

---
+++ PHP 3.php
@@ -0,0 +1,29 @@
+<!DOCTYPE>
+<html  lang =" es-US ">
+    <h1> Programación: Backend Developer </h1>
+    <br>
+    <h2> TRABAJO PRÁCTICO Nº3 </h2><hr>
+    <h2> Ejercios: </h2> <br>
+    Realizar un login  <br>
+<head>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="css/styles.css">
+    <title>Login</title>
+
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:400,700">
+
+</head>
+<body>
+    <div id="login">
+      <form action="/login.php" method="post">
+        <span class="fontawesome-user"></span>
+          <input type="text" id="user" placeholder="Username">
+
+        <span class="fontawesome-lock"></span>
+          <input type="password" id"pass" placeholder="Password">
+
+        <input type="submit" value="Login">
+
+</form>
+ </body>
+</html>
